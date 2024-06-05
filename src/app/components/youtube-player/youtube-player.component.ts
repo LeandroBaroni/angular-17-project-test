ad8f655d8dbe715a50434ac5e88598d1d1d11920
Leandro Baroni <leandrobaroni2103@hotmail.com>
2024-06-05 15:22:33 -0300

    add youtube player module

--- src/app/components/youtube-player/youtube-player.component.ts
+++ src/app/components/youtube-player/youtube-player.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-youtube-player',
   standalone: true,
-  imports: [],
+  imports: [YouTubePlayerModule],
+  providers: [],
   templateUrl: './youtube-player.component.html',
   styleUrl: './youtube-player.component.scss'
 })
